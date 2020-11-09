53de528cd35d5983d6a50221c4208f77a6256c4a
ShmyhelskyiSerhii <siro181a@gmail.com>
2020-11-09 15:56:05 +0200

    fixed header

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,6 @@
 let list = document.querySelectorAll('.menu__list-item');
 let shop = document.querySelector('.header__right-circle');
 let count = 0;
-console.dir(shop);
-console.log(list);
 
 list.forEach((elem) => {
     elem.addEventListener('click', () => {
