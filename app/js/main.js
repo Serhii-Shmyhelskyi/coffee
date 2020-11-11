1668cc77b4970b915112b874916b2f9f72574c63
ShmyhelskyiSerhii <siro181a@gmail.com>
2020-11-11 15:00:41 +0200

    added section favorite-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,12 @@
 let list = document.querySelectorAll('.menu__list-item');
-let shop = document.querySelector('.header__right-circle');
+let shop = document.querySelector('.header__menu-circle');
 let count = 0;
 
 list.forEach((elem) => {
     elem.addEventListener('click', () => {
-        count++
-        shop.innerHTML = `${count}`
+        count++;
+        count > 0 ? shop.classList.add('visible') : true;
+        shop.innerHTML = count;
     });
 
 })
